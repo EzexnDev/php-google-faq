8122bdcde6730156f46b96b6c01a655c002ed5f9
Gianmarco <gianmarco.cavaliere19093@gmail.com>
2021-01-06 10:51:06 +0100

    Done

---
+++ index.php
@@ -0,0 +1,271 @@
+<?php
+
+$db = [
+    [
+      'question' => 'Come state implementando la recente decisione della Corte
+                     di giustizia dell\'Unione europea (CGUE) relativa al diritto
+                     all\'oblio?',
+      'answer' => 'La recente <span class="color-blue">decisione della Corte di giustizia dell\'Unione
+                   europea</span> ha profonde conseguenze per i motori di ricerca in Europa.
+                   La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere
+                   ai motori di ricerca come Google di rimuovere risultati relativi a
+                   chiavi di ricerca che includono il proprio nome. Per poter essere
+                   rimossi, i risultati visualizzati devono essere inadeguati,
+                   irrilevanti o non più rilevanti, o eccessivi.
+                  <br><br>
+                   Da quando questa decisione è stata pubblicata il 13
+                   maggio 2014, abbiamo lavorato incessantemente per
+                   uniformarci a essa. Si tratta di una procedura complessa
+                   perché dobbiamo valutare ogni singola richiesta e
+                   effettuare un bilanciamento tra il diritto dell\'individuo
+                   a controllare i suoi dati personali ed il diritto di tutti
+                   di conoscere e distribuire le informazioni.
+                   <br><br>
+                   Per presentare una richiesta di rimozione, compila
+                   questo <span class="color-blue">modulo web</span>. Riceverai una risposta automatica
+                   che conferma la ricezione della tua richiesta.
+                   Dopodiché valuteremo il tuo caso (tieni presente che
+                   potrebbe occorrere un po\' di tempo perché abbiamo già
+                   ricevuto tante richieste simili). Nel valutare la
+                   richiesta, considereremo se i risultati includono
+                   informazioni obsolete relative alla tua vita privata.
+                   Considereremo inoltre se vi è un interesse pubblico a
+                   che le informazioni rimangano nei nostri risultati delle
+                   ricerche, ad esempio se riguardano frodi finanziarie,
+                   negligenza professionale, condanne penali o la tua condotta
+                   pubblica in relazione a un pubblico ufficio
+                   (eletto o non eletto). Queste valutazioni sono complesse
+                   e, in quanto organizzazione privata, potremmo non essere
+                   nella posizione giusta per prendere decisioni in merito
+                   al tuo caso. Se non sei d\'accordo con la nostra valutazione,
+                   puoi rivolgerti all\'Autorità garante per la
+                   protezione dei dati personali nel tuo paese.
+                   <br><br>
+                   Nei prossimi mesi lavoreremo a stretto contatto con le
+                   autorità per la protezione dei dati e con altre autorità
+                   per perfezionare il nostro approccio. La decisione della
+                   CGUE rappresenta un cambiamento significativo per i motori
+                   di ricerca. Siamo preoccupati per le sue conseguenze, ma
+                   riteniamo anche che sia importante rispettare la decisione
+                   della Corte e ci stiamo adoperando per predisporre una
+                   procedura conforme alla legge.
+                   <br><br>
+                   Quando cerchi un nome, potresti trovare una notifica
+                   in cui viene spiegato che i risultati potrebbero essere
+                   stati modificati nel rispetto delle leggi europee per
+                   la protezione dei dati. Mostriamo questa notifica agli
+                   utenti europei quando cercano la maggior parte dei nomi,
+                   non soltanto le pagine che sono state soggette a una rimozione.',
+    ],
+    [
+      'question' => 'Come fa Google a proteggere la mia privacy e a tenere
+                      le mie informazioni al sicuro?',
+      'answer' => 'Sappiamo che la sicurezza e la privacy sono importanti
+                    per te e lo sono anche per noi. Per noi è imperativo offrire un\'elevata
+                    sicurezza e assicurarti che le tue informazioni sono al sicuro e
+                    accessibili quando ne hai bisogno.
+                    <br><br>
+                    Siamo costantemente al lavoro per garantire un\'elevata sicurezza,
+                    proteggere la tua privacy e rendere Google ancora più efficace ed
+                    efficiente per te. Spendiamo centinaia di milioni di dollari ogni
+                    anno per la sicurezza e ci avvaliamo di esperti di fama mondiale
+                    in materia di sicurezza dei dati per mantenere le tue informazioni
+                    al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e
+                    la privacy di facile utilizzo come Google Dashboard, la verifica in
+                    due passaggi e Impostazioni annunci. Così, per quanto riguarda le
+                    informazioni che condividi con Google, hai il massimo controllo.
+                    <br><br>
+                    Puoi leggere ulteriori informazioni sulla sicurezza online, incluso
+                    come proteggere te e la tua famiglia online, nel
+                    <span class="color-blue">Centro Google perla sicurezza online</span>.
+                    <br><br>
+                    <span class="color-blue">Scopri</span> quali misure adottiamo per garantire protezione e sicurezza
+                    alle tue informazioni personali, lasciando a te il controllo.',
+    ],
+    [
+      'question' => 'Come faccio a rimuovere informazioni su di me dai
+                      risultati di ricerca di Google?',
+      'answer' => 'I risultati di ricerca di Google rispecchiano i contenuti
+                    pubblicamente disponibili sul Web. I motori di ricerca
+                    non possono rimuovere i contenuti direttamente dai siti
+                    web, quindi rimuovere risultati di ricerca da Google non
+                    consente di rimuovere i contenuti dal Web. Se desideri
+                    rimuovere qualcosa dal Web, devi
+                    <span class="color-blue">contattare il webmaster</span>
+                    del sito su cui sono pubblicati i contenuti e chiedergli
+                    di apportare una modifica. Inoltre, se, ai sensi delle
+                    leggi europee per la protezione dei dati, desideri
+                    richiedere la rimozione di determinate informazioni su
+                    di te visualizzate nei risultati di ricerca di Google,
+                    <span class="color-blue">fai clic qui</span>.
+                    Una volta che i contenuti saranno stati
+                    rimossi e che Google avrà rilevato l\'aggiornamento,
+                    le informazioni non verranno più visualizzate nei
+                    risultati di ricerca di Google. In caso di una richiesta
+                    di rimozione urgente, è inoltre possibile
+                    <span class="color-blue">visitare lanostra pagina di
+                    assistenza per avere ulteriori informazioni</span>.',
+    ],
+    [
+      'question' => 'Quando faccio clic sui risultati della Ricerca Google,
+                      le mie chiavi di ricerca vengono inviate ai siti web?',
+      'answer' => 'In alcuni casi sì. Quando fai clic su un risultato della
+                    Ricerca Google, il tuo browser web potrebbe reindirizzare
+                    alla pagina web di destinazione anche l\'indirizzo Internet,
+                    o URL, della pagina dei risultati di ricerca sotto forma di
+                    <span class="color-blue">URL referrer</span>.
+                    Talvolta, l\'URL della pagina dei risultati
+                    di ricerca potrebbe contenere la query di ricerca che hai
+                    inserito. Se utilizzi la ricerca SSL (la funzione di
+                    ricerca criptata di Google), nella maggior parte dei casi
+                    i termini di ricerca non vengono inviati come parte
+                    dell\'URL negli URL referrer. Questo comportamento può
+                    fare eccezione, ad esempio se utilizzi alcuni browser
+                    meno diffusi. Ulteriori informazioni sulla ricerca SSL
+                    sono disponibili <span class="color-blue">qui</span>.
+                    Le query di ricerca o le informazioni
+                    contenute nell\'URL referrer potrebbero essere disponibili
+                    mediante Google Analytics o un\'API (Application Programming
+                    Interface). Inoltre, gli inserzionisti potrebbero ricevere
+                    informazioni relative all\' esatte parole chiave che hanno
+                    determinato il clic su un annuncio.',
+    ],
+  ];
+?>
+<style>
+    <?php include 'css/style.css';
+    ?>
+</style>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privacy e Termini - Google</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <div class="logo2">
+                    <img src="google-logo.svg" alt="">
+                    <span>Privacy e termini</span>
+                </div>
+                <div class="contact">
+                    <img src="profile-pic.png" alt="Logo-Profile">
+                    <i class="fas fa-grip-horizontal"></i>
+                </div>
+            </div>
+            <div class="menu">
+                <ul class="list-menu">
+                    <li>Introduzione</li>
+                    <li>Norme sulla privacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnologie</li>
+                    <li class="active">Domande frequenti</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="contenent">
+        <?php
+            foreach ($db as $question_and_answer => $textList) {
+                echo '<h2>'.$textList['question'].'<h3>';
+                echo '<p>'.$textList['answer'].'</p>';
+            }
+        ?>
+        </div>
+    </main>
+    
+    <!-- FOOTER -->
+    <footer>
+      <div class="container">
+        <div class="info">
+          <ul class="info-list">
+            <li>Google <span>-</span> </li>
+            <li>Tutto su Google <span>-</span> </li>
+            <li>Privacy <span>-</span> </li>
+            <li>Termini</li>
+          </ul>
+        </div>
+
+        <div class="language">
+          <i class="fas fa-comment-alt"></i>
+          <select class="lang">
+            <option>Italiano</option>
+            <option>Afrikaans</option>
+            <option>Bahasa Indonesia</option>
+            <option>Bahasa Melayu</option>
+            <option>Català</option>
+            <option>Čeština</option>
+            <option>Dansk</option>
+            <option>Deutsch</option>
+            <option>Eesti</option>
+            <option>English</option>
+            <option>English (United Kingdom)</option>
+            <option>Español</option>
+            <option>Español (Latinoamérica)</option>
+            <option>Euskara</option>
+            <option>Filipino</option>
+            <option>Français</option>
+            <option>Français (Canada)</option>
+            <option>Galego</option>
+            <option>Hrvatski</option>
+            <option>Isizulu</option>
+            <option>Íslenska</option>
+            <option>Kiswahili</option>
+            <option>Latviešu</option>
+            <option>Lietuvių</option>
+            <option>Magyar</option>
+            <option>Nederlands</option>
+            <option>Norsk</option>
+            <option>Polski</option>
+            <option>Português (Brasil)</option>
+            <option>Português (Portugal)</option>
+            <option>Română</option>
+            <option>Slovenčina</option>
+            <option>Slovenščina</option>
+            <option>Srpski</option>
+            <option>Suomi</option>
+            <option>Svenska</option>
+            <option>Tiếng Việt</option>
+            <option>Türkçe</option>
+            <option>Ελληνικά</option>
+            <option>Български</option>
+            <option>Русский</option>
+            <option>Српски</option>
+            <option>Українська</option>
+            <option>&#8235;עברית&#8236;</option>
+            <option>&#8235;اردو&#8236;</option>
+            <option>&#8235;العربية&#8236;</option>
+            <option>&#8235;فارسی&#8236;</option>
+            <option>አማርኛ</option>
+            <option>मराठी</option>
+            <option>हिन्दी</option>
+            <option>বাংলা</option>
+            <option>ગુજરાતી</option>
+            <option>தமிழ்</option>
+            <option>తెలుగు</option>
+            <option>ಕನ್ನಡ</option>
+            <option>മലയാളം</option>
+            <option>ไทย</option>
+            <option>한국어</option>
+            <option>中文 (香港)</option>
+            <option>中文（简体中文）</option>
+          </select>
+        </div>
+      </div>
+    </footer>
+
+
+</body>
+
+</html>
